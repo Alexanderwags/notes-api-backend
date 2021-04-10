a9ae30009b4cba08349d0a07ff14abed057bdb51
william_ws7 🔥 <alexander171296@gmail.com>
2021-04-10 11:33:45 -0400

    feat: :art: it was added bcrypt for encriptacion

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,18 @@
 const usersRouter = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-usersRouter.post("/api/users", async (request, response) => {
+usersRouter.post("/", async (request, response) => {
   const {body} = request;
   const {userName, name, password} = body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
     userName,
     name,
-    passwordHash: password,
+    passwordHash,
   });
 
   const saveUser = await user.save();
